203300d6f4b2f286cfbdad24c11f395f70b31b64
Jake Garcia <bballwiz4@gmail.com>
2024-05-19 16:04:43 -0600

    Added in a time based greeting and a motivational text that will display a random message.

--- components/home/greeting.jsx
+++ components/home/greeting.jsx
@@ -3,13 +3,30 @@ import { View, StyleSheet, Text, } from 'react-native';
 
 const Greeting = () => {
 
-    const dynamicGreeting = () => {
-        return "Good Afternoon"
+    const dynamicTimeGreeting = () => {
+        let today = new Date();
+        let hour = today.getHours();
+        let timeGreeting;
+        if (hour < 12) {
+            timeGreeting = "Good Morning";
+        } else if (hour > 11 && hour < 16) {
+            timeGreeting = "Good Afternoon";
+        } else {
+            timeGreeting = "Good Evening";
+        }
+        return timeGreeting;
+    };
+
+    const motivationText = () => {
+        let texts = ["Get after it!", "Kill your workout!", "Lift those weights!"];
+        let randomIndex = Math.floor(Math.random() * texts.length);
+        return texts[randomIndex];
     }
 
     return (
         <View style={styles.greetingContainer}>
-            <Text>{dynamicGreeting()}</Text>
+            <Text>{dynamicTimeGreeting()}</Text>
+            <Text>{motivationText()}</Text>
         </View>
     )
 }
